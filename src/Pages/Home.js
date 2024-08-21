e62d892242e3538fab53c0b46e4ccae2321907ac
sbhagat101 <shivbhagat42@gmail.com>
2024-08-20 21:37:46 -0700

    updated hero section for home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,36 +1,35 @@
-import React, { useRef, useEffect } from "react";
 import profile_pic from "../assets/profile_pic.png"
 import Typewriter from "typewriter-effect"
 
 function Home() {
-  const myimageref = useRef();
 
   return (
-    <section class="text-gray-600 body-font h-screen flex bg-svg-constellation-gray-100 relative">
-        <div class="container mx-auto flex px-5 py-12 items-center justify-center flex-col">
-            <div class="lg:w-2/3 w-full animate-fade-in-down">
-            
-            <h1 class="md:text-6xl text-3xl mb-2 font-bold text-white tracking-tight leading-tight">
-                Hi, my name is
+    <main className="container mx-auto px-4 md:px-8 xl:max-w-screen-lg section flex flex-col md:flex-row justify-between items-center">
+        <div className="text-center md:text-left md:w-1/2">
+            <h1 className="text-2xl bg-clip-text bg-gradient2 text-transparent md:text-4xl xl:text-5xl xl:leading-tight font-bold">
+            Hi, my name is
             </h1>
-            
-            <h1 class="text-2xl bg-clip-text bg-gradient text-transparent md:text-6xl xl:text-6xl xl:leading-snug font-bold">
-                Shiv Bhagat
+            <h1 className="text-2xl bg-clip-text bg-gradient text-transparent md:text-4xl xl:text-5xl xl:leading-tight font-bold">
+            Shiv Bhagat
             </h1>
-            
-            <p class="mt-8 mb-16 md:leading-tight leading-normal tracking-tight text-xl text-white">
+            <div className="text-2xl bg-clip-text bg-gradient2 text-transparent md:text-4xl xl:text-5xl xl:leading-tight font-bold ">
                 <Typewriter
                 options={{
-                strings: ['Full Stack Developer', 'Programmer', 'Software Engineer', 'Car Enthusiast', 'Sport Enthusiast'],
+                strings: [
+                    'Full Stack Developer',
+                    'Programmer',
+                    'Software Engineer',
+                    'Car Enthusiast',
+                    'Sport Enthusiast',
+                ],
                 autoStart: true,
                 loop: true,
                 }}
                 />
-            </p>
-            
-            <a class="uppercase rounded-sm bg-gradient font-bold text-white px-8 py-4 mx-auto mr-4 hidden md:inline"
-                href="/contact">Contact Me</a>
             </div>
+        </div>
+        <div className="mt-8 md:mt-0 md:w-1/2 flex justify-center md:justify-end">
+            <img className="w-3/4 md:w-1/2 lg:w-2/5" src={profile_pic} alt="Shiv Bhagat" />
         </div>
         <a href="/about" class="absolute block mx-auto text-xl md:mt-96 bottom-0 left-1/2 -ml-10 mt-80">
             <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -39,7 +38,7 @@ function Home() {
                 fill="#FFFFFF"></path>
             </svg>
         </a>
-    </section>
+    </main>
 
 
   );
