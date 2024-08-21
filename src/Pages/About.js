45dca4afdbadcb65f3a72bd14c6e522c4dceb013
sbhagat101 <shivbhagat42@gmail.com>
2024-08-20 21:56:48 -0700

    Updated about me content

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -12,10 +12,11 @@ function About() {
             <p className="text-content bg-clip-text bg-gradient2 text-transparent py-8 lg:max-w-3xl">
             Hello! My name is Shiv a software engineer early in his career.   
             My programming journey started back in high school when i took an computer science class where I learned Python.
-            Fast-Forward to today, and I've had the privilege of working for one biggest defense Contractor in Raytheon.
+            Fast-Forward to today, and I've had the privilege of working for one biggest defense Contractor in Raytheon. 
+            Actively seeking new opportunities in Southern California.
             </p>
         </section>
-        
+
         <section>
             <h1 className="text-2xl bg-clip-text bg-gradient2 text-transparent md:text-4xl xl:text-5xl xl:leading-tight font-bold mb-10">Work Experiences & Education</h1>
             <div class="relative border-l border-gray-200">
