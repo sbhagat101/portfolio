13ebeb34c4b1b971feec4b9e6ed85f8b2f9b6b5b
sbhagat101 <shivbhagat42@gmail.com>
2024-08-22 12:45:22 -0700

    updated path again

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -32,7 +32,7 @@ function Home() {
             <img className="w-3/4 md:w-1/2 lg:w-2/5" src={profile_pic} alt="Shiv Bhagat" />
         </div>
 
-        <a href="/portfolio/about" class="absolute block mx-auto text-xl md:mt-96 bottom-0 left-1/2 -ml-10 mt-80">
+        <a path="/about" class="absolute block mx-auto text-xl md:mt-96 bottom-0 left-1/2 -ml-10 mt-80">
             <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" clip-rule="evenodd"
                 d="M21.1716 29.1716C22.7337 27.6095 25.2663 27.6095 26.8284 29.1716L40 42.3431L53.1716 29.1716C54.7337 27.6095 57.2663 27.6095 58.8284 29.1716C60.3905 30.7337 60.3905 33.2663 58.8284 34.8284L42.8284 50.8284C41.2663 52.3905 38.7337 52.3905 37.1716 50.8284L21.1716 34.8284C19.6095 33.2663 19.6095 30.7337 21.1716 29.1716Z"
