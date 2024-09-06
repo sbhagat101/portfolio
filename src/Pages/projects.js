4d71d55df86c542d7e4fbb8e73daa23d6ca18ced
sbhagat101 <shivbhagat42@gmail.com>
2024-09-05 22:51:55 -0700

    Added Updated Resume and a New Project

--- src/Pages/projects.js
+++ src/Pages/projects.js
@@ -3,6 +3,8 @@ import Project from "../Components/Project";
 import mandirs from "../assets/project/mandirs.jpg"
 import portfolio from "../assets/project/portfolio.jpg"
 import titanTalks from "../assets/project/TitanTalks.jpg"
+import weather from "../assets/project/weather.jpg"
+
 function Projects() {
     const projectDetails = [
         {
@@ -18,9 +20,17 @@ function Projects() {
             image: mandirs,
             description: `This is an ios app which showcases BAPS Temples across North America`,
             techstack: "Swift",
-            previewLink: "https://google.com",
-            githubLink: "https://github.com",
-          },
+            previewLink: "https://github.com/sbhagat101/Mandirs?tab=readme-ov-file#simulation",
+            githubLink: "https://github.com/sbhagat101/Mandirs",
+        },
+        {
+          title: "Weather Dashboard",
+          image: weather,
+          description: `An Weather Dashboard to view weather in any given city and also view the next 5 day forecast`,
+          techstack: "React JS, HTML, CSS, Axios",
+          previewLink: "https://sbhagat101.github.io/WeatherDashboard/",
+          githubLink: "https://github.com/sbhagat101/WeatherDashboard",
+        },
         {
           title: "Titan Talks",
           image: titanTalks,
@@ -34,7 +44,7 @@ function Projects() {
   return (
     <main className="container mx-auto max-width pt-10 mb-20">
       <section>
-        <h1 className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
+        <h1 className="text-2xl bg-clip-text bg-gradient2 text-transparent md:text-4xl xl:text-5xl xl:leading-tight font-bold">
           Projects
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-10">
